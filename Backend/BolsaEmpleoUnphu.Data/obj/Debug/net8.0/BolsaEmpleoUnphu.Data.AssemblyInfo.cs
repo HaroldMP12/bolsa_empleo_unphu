eb32102407832a99d262ab38b55fecf1aec3b692
Harold Morillo Pichardo <hmorillopichardo@gmail.com>
2025-07-29 23:03:52 -0400

    Subiendo al repo

--- Backend/BolsaEmpleoUnphu.Data/obj/Debug/net8.0/BolsaEmpleoUnphu.Data.AssemblyInfo.cs
+++ Backend/BolsaEmpleoUnphu.Data/obj/Debug/net8.0/BolsaEmpleoUnphu.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BolsaEmpleoUnphu.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675fb67425aa1ce054bef1563f0fece8cf433e89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eba15a2249c87894338a4db59acb2c33a9b6e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("BolsaEmpleoUnphu.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BolsaEmpleoUnphu.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
