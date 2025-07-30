d7ef3016d1af577acef33f6a2da5f2579f4f4aa9
Harold Morillo Pichardo <hmorillopichardo@gmail.com>
2025-07-30 00:10:12 -0400

    Revert "Resolviendo conflictos de conexion a bd y error de referencia"
    This reverts commit 044e9fa9a94ad9251273ea4f336e426e90148c94.

--- Backend/BolsaEmpleoUnphu.API/Program.cs
+++ Backend/BolsaEmpleoUnphu.API/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using BolsaEmpleoUnphu.Data.Context;
-using System.Text.Json.Serialization;
 
 namespace BolsaEmpleoUnphu.API
 {
@@ -15,19 +14,8 @@ namespace BolsaEmpleoUnphu.API
             // Configuración de la conexión a la base de datos
             builder.Services.AddDbContext<BolsaEmpleoUnphuContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Configuración para evitar referencias circulares en JSON
-            builder.Services.ConfigureHttpJsonOptions(options =>
-            {
-                options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-            });
-
-            builder.Services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-                });
-
+                
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -44,6 +32,7 @@ namespace BolsaEmpleoUnphu.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
 
             app.MapControllers();
 
